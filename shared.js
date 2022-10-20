2c61368885ccb32770a9ca3f8b59b965dced7201
OlenaPetrova2002 <olena.petrova.2002@gmail.com>
2022-10-20 13:07:33 +0300

    toggle-botton- 123-1

--- shared.js
+++ shared.js
@@ -1,27 +1,33 @@
+'use strict';
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
-var toggleButton = document.querySelector('.toggle-botton');
+var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav'); 
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    //modal.style.display = 'block';
+    //backdrop.style.display = 'block';
+    //modal.className = 'open';
+    modal.classList.add('open');
 });
 }
   
-  backdrop.addEventListener("click", function() {
+  backdrop.addEventListener('click', function() {
     mobileNav.style.display = 'none';
     closeModal();
   });
   
-    modalNoButton.addEventListener("click", closeModal);
+    modalNoButton.addEventListener('click', closeModal);
   
   function closeModal() {
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+    //backdrop.style.display = 'none';
+    //modal.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
   }
   
   toggleButton.addEventListener('click', function() {
